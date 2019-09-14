8253a3e9ca7b43f9ef6f46a5a736f5fd78578c75
Oisín Mac Fhearaí <denpashogai@gmail.com>
2019-09-14 17:21:20 +0100

    Switch to SQLite for local build too

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ APPNAME=$1
 
 rm -rf target && mkdir target
 cd src
-urweb $1 -db "dbname=memvalid_ur host=localhost user=postgres password=$SECRET_DB_PASSWD" -output ../target/$1.exe -endpoints ../target/endpoints.json
+# interestingly, the path supplied to the "-db" argument is relative to
+# where the app will be running from, NOT necessarily the directory we're
+# running the compiler from.
+urweb $1 -dbms sqlite -db target/db.sqlite -output ../target/$1.exe -endpoints ../target/endpoints.json
