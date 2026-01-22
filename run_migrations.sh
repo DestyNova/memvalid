24d53e2f4fef9f1a47d9cd9626a6e4e78bfef2d2
Oisín Mac Fhearaí <denpashogai@gmail.com>
2026-01-22 22:57:46 +0000

    Rename a script + rm "migrations" script that just tries to throw away a Postgres DB

--- run_migrations.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# careful now...
-(echo "drop database if exists memvalid_ur; create database memvalid_ur; \c memvalid_ur" && cat src/memvalid.sql) | psql -U postgres -h localhost -f -
